464ac013bbfdfd8bafdc6b675f2508d9f9f31c0f
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-10-20 11:56:29 +0530

    Update app.js

--- Problem 1/app.js
+++ Problem 1/app.js
@@ -4,32 +4,46 @@ document.addEventListener("DOMContentLoaded", function () {
   const responseDiv = document.getElementById("response");
 
   tweetForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+    // to prevent the default behavior of an HTML element when an event is triggered. It's commonly used in event handlers, such as those for form submissions(Avoid full reload) or links, to control how the browser responds to an event.
+    event.preventDefault(); 
 
     const tweet = tweetText.value;
-
-    // Make an HTTP POST request to the backend
-    fetch("https://one00x-data-analysis.onrender.com/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ post: { content: tweet } }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json(); // Parse response as JSON
-        } else {
-          throw new Error("Tweet posting failed"); // Handle failure
-        }
-      })
-      .then((data) => {
-        // Handle successful tweet post
-        responseDiv.innerText = `Tweet posted successfully! Tweet ID: ${data.id}`;
-      })
-      .catch((error) => {
-        // Handle error
-        responseDiv.innerText = `Error: ${error.message}`;
-      });
+    postTweet(tweet, responseDiv); 
   });
 });
+
+function postTweet(tweetContent, responseDiv) {
+  // Make an HTTP POST request to the backend
+  fetch("https://one00x-data-analysis.onrender.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ post: { content: tweetContent } }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); // Parse response as JSON
+      } else {
+        throw new Error("Tweet posting failed");// Handle failure
+      }
+    })
+    .then((data) => {
+      // Handle successful tweet post
+      handleSuccessfulPost(data, responseDiv); // Handle successful tweet post
+    })
+    .catch((error) => {
+      // Handle error
+      handlePostError(error, responseDiv); // Handle error
+    });
+}
+
+function handleSuccessfulPost(data, responseDiv) {
+  responseDiv.innerText = `Tweet posted successfully! Post ID: ${data.id}`;
+}
+
+function handlePostError(error, responseDiv) {
+  responseDiv.innerText = `Error: ${error.message}`;
+}
+
+
